7e567ed4dda9c3c539ef8ed22a037020b4e66a13
Daniele <134394136+DanieleAnjos@users.noreply.github.com>
2024-11-28 20:52:58 -0300

    at4

--- config/database.js
+++ config/database.js
@@ -1,21 +1,34 @@
-// config/sequelize.js
-
 require('dotenv').config();  // Carrega as variáveis do arquivo .env
+const { execSync } = require('child_process');
+
+// Função para verificar e instalar pacotes necessários
+const ensurePackageInstalled = (packageName) => {
+  try {
+    require.resolve(packageName);
+    console.log(`${packageName} já está instalado.`);
+  } catch (error) {
+    console.log(`Instalando ${packageName}...`);
+    execSync(`npm install ${packageName}`, { stdio: 'inherit' });
+  }
+};
+
+// Garantir que o pacote `tedious` esteja instalado
+ensurePackageInstalled('tedious');
 
 const { Sequelize } = require('sequelize');
 
 // Usar variáveis de ambiente para credenciais sensíveis
 const sequelizeConfig = {
-  host: process.env.DB_HOST , // Host do banco de dados na Azure
-  port: process.env.DB_PORT ,  // Porta padrão para SQL Server
-  database: process.env.DB_NAME , 
-  username: process.env.DB_USER ,           
-  password: process.env.DB_PASSWORD,           
+  host: process.env.DB_HOST, // Host do banco de dados na Azure
+  port: process.env.DB_PORT, // Porta padrão para SQL Server
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   dialect: 'mssql', // Dialeto para SQL Server
   dialectOptions: {
     options: {
-      encrypt: true,  // Azure exige criptografia na conexão
-      trustServerCertificate: process.env.NODE_ENV === 'production' ? false : true,  // Ajuste conforme o ambiente
+      encrypt: true, // Azure exige criptografia na conexão
+      trustServerCertificate: process.env.NODE_ENV === 'production' ? false : true, // Ajuste conforme o ambiente
     }
   }
 };
