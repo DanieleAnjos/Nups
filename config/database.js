6fcd8e973a62514ee5b0613c9c7f2f639f287b17
Daniele <134394136+DanieleAnjos@users.noreply.github.com>
2025-03-04 19:29:07 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`  // Carrega o arquivo correto dependendo do ambiente
+});
 const { execSync } = require('child_process');
 
 /**
