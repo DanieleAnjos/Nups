84e1254604f56cbfdd17080436cbe43b718a9871
Daniele <134394136+DanieleAnjos@users.noreply.github.com>
2025-03-04 20:04:27 -0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,23 @@
 module.exports = {
   apps: [
     {
-      name: "nups-prod",               // Nome do processo para produção
-      script: "npm",                   // Rodar o npm como script
-      args: "run start",               // Executar o script 'start' do package.json
-      env: {                           // Variáveis de ambiente para o ambiente de desenvolvimento
-        NODE_ENV: "production",       // Definir o ambiente como produção
-        DOMAIN: "nupsweb.org",        // Domínio para o ambiente de produção
-        PORT: 4000                    // Porta para o ambiente de produção
+      name: "nups-prod",
+      script: "node",
+      args: "app.js", // ou qualquer outro script de inicialização
+      env: {
+        NODE_ENV: "production",
+        DOMAIN: "nupsweb.org",
+        PORT: 4000
       }
     },
     {
-      name: "nups-dev",                // Nome do processo para desenvolvimento
-      script: "npm",                   // Rodar o npm como script
-      args: "run dev",                 // Executar o script 'dev' do package.json
+      name: "nups-dev",
+      script: "nodemon",
+      args: "app.js",
       env: {
-        NODE_ENV: "development",      // Definir o ambiente como desenvolvimento
-        DOMAIN: "dev.nupsweb.org",    // Domínio do ambiente de desenvolvimento
-        PORT: 3000                     // Porta para o ambiente de desenvolvimento
+        NODE_ENV: "development",
+        DOMAIN: "dev.nupsweb.org",
+        PORT: 3000
       }
     }
   ]
