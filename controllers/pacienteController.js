51befad7ae321a3d8f1380a67411b2cd4e425868
Daniele <134394136+DanieleAnjos@users.noreply.github.com>
2025-03-23 20:45:35 -0300

    .

--- controllers/pacienteController.js
+++ controllers/pacienteController.js
@@ -551,6 +551,8 @@ exports.delete = async (req, res) => {
     res.status(500).send('Erro ao deletar paciente. Tente novamente mais tarde.');
   }
 };
+
+
 exports.perfil = async (req, res) => {
   const { id } = req.params;
 
@@ -595,14 +597,29 @@ exports.perfil = async (req, res) => {
 
     const atendimentosFiltrados = paciente.atendimentos.filter(atendimento => {
       const atendimentoCargo = atendimento.profissional.cargo.toLowerCase();
-    
-      return (
-        cargo === "administrador" ||
-        atendimento.profissionalId === profissional.id ||
-        (isGestor && gestorCargosMap[cargo].includes(atendimentoCargo)) ||
-        (['assistente social', 'psicólogo', 'psiquiatra'].includes(atendimentoCargo) &&
-          Object.values(gestorCargosMap).flat().includes(cargo))
-      );
+
+      // Administrador pode ver tudo
+      if (cargo === "administrador") return true;
+
+      // Profissional pode ver seus próprios atendimentos
+      if (atendimento.profissionalId === profissional.id) return true;
+
+      // Gestor pode ver atendimentos dos profissionais sob sua gestão
+      if (isGestor && gestorCargosMap[cargo].includes(atendimentoCargo)) return true;
+
+      // Profissional pode ver atendimentos do seu respectivo gestor
+      if (
+        ['assistente social', 'psicólogo', 'psiquiatra'].includes(cargo) &&
+        Object.keys(gestorCargosMap).some(gestorCargo => {
+          return gestorCargosMap[gestorCargo].includes(cargo) && // O profissional pertence à gestão do gestor
+                 atendimentoCargo === gestorCargo; // O atendimento foi realizado pelo gestor
+        })
+      ) {
+        return true;
+      }
+
+      // Caso contrário, não pode ver o atendimento
+      return false;
     });
 
     const podeEditar = cargo === "administrador" || 
@@ -633,7 +650,6 @@ exports.perfil = async (req, res) => {
     return res.status(500).send(`Erro ao buscar perfil do paciente: ${error.message}`);
   }
 };
-
 
 exports.relatorioDetalhes = async (req, res) => {
   const { id } = req.params;
